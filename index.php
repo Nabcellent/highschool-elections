30997c6d200dadbe7d85eaa0c8a620c297e25acb
Red-Beard7 <michael.nabangi@strathmore.edu>
2020-12-12 19:35:47 +0300

    index mod

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $link = connect_to_db();
 ?>
 
 <div class="jumbotron jumbotron-top text-center">
-    <h1>MAIN MENU</h1>
+    <h1>WELCOME TO MAIN MENU</h1>
     <p>Your vote counts! Select your leaders!</p>
 </div>
 
